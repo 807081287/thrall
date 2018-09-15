83302bc7ce037ec9e37d7fb23cb53aef03c439d8
Huyd <huyidada@gmail.com>
2018-09-15 12:22:09 +0800

    Initializing server project

---
+++ thrall-server/src/main/java/com/thrall/ThrallServerApplication.java
@@ -0,0 +1,12 @@
+package com.thrall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThrallServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThrallServerApplication.class, args);
+    }
+}
