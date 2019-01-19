d5af601bdade618366d7e2504b2c180808aa9caf
huyida <huyidada@163.com>
2019-01-19 15:28:31 +0800

    教师模块和学生模块

--- thrall-server/src/main/java/com/thrall/web/StudentController.java
+++ thrall-server/src/main/java/com/thrall/web/StudentController.java
@@ -7,11 +7,9 @@ import com.thrall.service.UserinfoService;
 import com.thrall.util.SHA256Str;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.ibatis.annotations.Param;
 import org.apache.shiro.authz.annotation.Logical;
 import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
@@ -20,14 +18,14 @@ import java.util.List;
 
 /**
  * @program: thrall-server
- * @description: 管理员信息操作
+ * @description: 学生信息操作
  * @author: huyida
- * @create: 2019-01-17 23:05
+ * @create: 2019-01-19 15:21
  **/
-@Api(value = "UserinfoController", description = "管理员信息模块")
+@Api(value = "StudentController", description = "学生信息模块")
 @RestController
-@RequestMapping("/admin")
-public class UserinfoController {
+@RequestMapping("/student")
+public class StudentController {
     @Autowired
     UserinfoService userinfoService;
 
@@ -35,60 +33,60 @@ public class UserinfoController {
         this.userinfoService = userinfoService;
     }
 
-    @ApiOperation(value = "添加用户", notes = "添加用户")
-    @PostMapping("/saveUser")
+    @ApiOperation(value = "添加学生", notes = "添加学生")
+    @PostMapping("/saveTeacher")
     @RequiresRoles("admin")
     public Result saveUser(@RequestBody Userinfo userinfo) {
-        userinfo.setRole("admin");
         Userinfo u = userinfoService.getByUsername(userinfo.getUsername());
-        if (u != null) {
+        if ("student".equals(u.getRole())) {
             return ResultGenerator.getFailResult("用户已存在");
         }
         userinfo.setPassword(SHA256Str.getSHA256StrJava(userinfo.getPassword()));//密码加密
+        userinfo.setRole("student");
         //获取当前时间，创建时间和更新时间
         Date date = new Date();
         Timestamp timeStamep = new Timestamp(date.getTime());
         userinfo.setCreate_date(timeStamep);
         userinfo.setUpdate_date(timeStamep);
-        int id = userinfoService.saveUserinfo(userinfo);//添加用户
+        int id = userinfoService.saveUserinfo(userinfo);//添加学生
         return ResultGenerator.getSuccessResult(id);
     }
 
-    @ApiOperation(value = "删除用户", notes = "删除用户")
-    @DeleteMapping("/removeUser")
+    @ApiOperation(value = "删除学生", notes = "删除学生")
+    @DeleteMapping("/removeTeacher")
     @RequiresRoles("admin")
     public Result removeUser(@RequestParam("id") int id) {
         Userinfo userinfo = new Userinfo();
         userinfo.setId(id);
-        userinfo.setRole("admin");
-        //获取用户
+        userinfo.setRole("student");//设置角色为学生
+        //获取学生
         List<Userinfo> u = userinfoService.getUserinfo(userinfo);
         //如果用户数量等于1条，那么存在此id数据
         if (u.size() != 1) {
-            return ResultGenerator.getFailResult("用户不存在");
+            return ResultGenerator.getFailResult("学生不存在");
         }
         //删除用户
         int result = userinfoService.removeUserinfo(id);
-        return ResultGenerator.getSuccessResult("用户删除成功");
+        return ResultGenerator.getSuccessResult("删除成功");
     }
 
-    @ApiOperation(value = "获取用户", notes = "根据查找的属性来获取用户")
-    @PostMapping("/getUser")
+    @ApiOperation(value = "获取学生", notes = "根据查找的属性来获取学生")
+    @PostMapping("/getTeacher")
     @RequiresRoles("admin")
-    public Result getUser(@RequestBody Userinfo userinfo) {
+    public Result getTeacher(@RequestBody Userinfo userinfo) {
         //根据对象中参数查询数据，如果对象为空，那么查询出所有用户
+        userinfo.setRole("student");
         List<Userinfo> u = userinfoService.getUserinfo(userinfo);
         return ResultGenerator.getSuccessResult(u);
     }
 
-    @ApiOperation(value = "更新用户", notes = "更新用户")
-    @PutMapping("/updateUser")
-    @RequiresRoles(value = {"user", "admin"}, logical = Logical.OR)
+    @ApiOperation(value = "更新学生", notes = "更新学生")
+    @PutMapping("/updateTeacher")
+    @RequiresRoles(value = {"student", "admin"}, logical = Logical.OR)
     public Result updateUser(@RequestBody Userinfo userinfo) {
-        //更新用户
+        //更新学生
+        userinfo.setRole("student");
         int result = userinfoService.updateUserinfo(userinfo);
         return ResultGenerator.getSuccessResult("更新成功");
     }
-
-
 }
